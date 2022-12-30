9a4645c7fe041c5d6b72f99fbfecb2e45aca1d5d
smart-programmer <albrns1123488@outlook.com>
2022-12-30 15:39:17 +0300

    initial commit

---
+++ typelearn/Main.java
@@ -0,0 +1,23 @@
+package typelearn;
+
+
+/*
+    descipriton(user story): a timed speed typing program where
+    words are shown to the user and he has to type them, each word 
+    is sperated by a space, the user can delete the letters that he typed in the current
+    word to correct himself but as soon as they press space to go 
+    to the next word they cant delete the previous one.
+    
+*/
+
+/*
+    task 1: write a txt file reader that reads words(space seperated text)
+    task 2: write a sentence perompter that prompts a different sentence from file on "ENTER" button press
+    task 3: write a timer tha starts from first sentence prompt and ends with last sentense prompt
+    task 4: write a 
+*/
+
+
+public class Main {
+    
+}
