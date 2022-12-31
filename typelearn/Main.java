126fb71bd2e4b85575f8f164d18f131886cd4eae
smart-programmer <albrns1123488@outlook.com>
2022-12-31 15:12:29 +0300

    added all main functionality

--- typelearn/Main.java
+++ typelearn/Main.java
@@ -1,31 +1,45 @@
 package typelearn;
 
 import java.util.Scanner;
-
-/*
-    descipriton(user story): a timed speed typing program where
-    words are shown to the users and they have to type them, each word 
-    is sperated by a space, the user can delete the letters that they typed in the current
-    word to correct themselves but as soon as they press space to go 
-    to the next word they cant delete the previous one.
-    
-*/
-
-/*
-    task 1: write a txt file reader that reads words(line seperated text) loads them into mem then
-    displays them as space seperated
-    task 2: write the typing functionality where user input is read and stored in an array
-    task 3: write a timer that starts from first sentence prompt and ends with last sentense typed
-    task 4: write the fault percentage functionality where the file words array is simply compared with
-    the user input array
-*/
+import java.util.Arrays;
 
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         WordsFileHandler handler = new WordsFileHandler();
         String[] words = handler.readWordsFile();
- 
-//        System.out.println(handler.filePathGetter());
+        
+        Scanner scanner = new Scanner(System.in);
+        String[] userWords = new String[words.length];
+        
+        // prompt user to start
+        System.out.println("put your cursor in the console then press any button to start");
+        scanner.nextLine();
+        // start timer after game start
+        double startTime = System.currentTimeMillis();
+        // main loop
+        for (int i = 0; i < words.length; i++){
+            System.out.println(words[i]);
+            String userWord = scanner.nextLine();  // Read user input
+            userWords[i] = userWord;
+        }
+        double endTime = System.currentTimeMillis();
+        double elapsedTimeInSeconds = ((endTime - startTime) / 1000);
+        double wordPerMinute = (userWords.length / (elapsedTimeInSeconds / 60)); // number of words / elapsed minutes
+        System.out.println("your spelling of the words was: " + Arrays.toString(userWords) + ".");
+        System.out.println("percentage of words you spelled wrong: " + spellingFaultPercentage(words, userWords) + ".");
+        System.out.println("your total time was: "  + elapsedTimeInSeconds + " seconds.");
+        System.out.println("your WPM(word per minute) is: "  + wordPerMinute + " words per minute.");
+    }
+    
+    
+    static double spellingFaultPercentage(String[] originalWordsArray, String[] userWordArray){
+        // calculate number of wrongly spelled words in user array devided by prompted number of words
+        int wronglySpelledWordsCount = 0;
+        for (int i = 0; i < originalWordsArray.length; i++){
+            if (!originalWordsArray[i].equals(userWordArray[i]))
+                wronglySpelledWordsCount++;
+        }
+        return (((double)wronglySpelledWordsCount / originalWordsArray.length) * 100);
     }
 }
